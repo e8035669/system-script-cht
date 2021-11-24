4830cd22f65a4df81edc0f82ccdf4d9c40095d21
Yu-Ren Zhang <e8035669@gmail.com>
2021-11-24 14:51:19 +0800

    fix curl proxy

--- install-docker.sh
+++ install-docker.sh
@@ -26,6 +26,7 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
+sudo cp -v ./docker-compose /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+sudo ln -v -s /usr/local/bin/docker-compose /usr/bin/docker-compose
